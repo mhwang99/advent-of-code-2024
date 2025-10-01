1caef663d196aab92f2abde1351f8aa41804c65a
Mark Hwang <hwangma@gmail.com>
2025-10-01 19:58:03 -0400

    day 8

--- src/aoc24/d08.clj
+++ src/aoc24/d08.clj
@@ -20,21 +20,6 @@
 ............"))
 (def in (get-line-chars))
 
-(defn get-pts
-  [board target]
-  (let [rows (count board)
-        cols (count (first board))]
-    (reduce
-      (fn [ret row]
-        (reduce
-          (fn [ret col]
-            (if (= target
-                   (get-in board [row col]))
-              (conj ret [row col])
-              ret))
-          ret (range cols)))
-      #{} (range rows))))
-
 (defn get-all-antenna
   [board]
   (let [rows (count board)
@@ -45,20 +30,15 @@
           (fn [ret col]
             (let [v (get-in board [row col])]
               (if (not= \. v)
-                (conj ret v)
+                (update ret v (fnil conj #{}) [row col])
                 ret)))
           ret (range cols)))
-      #{} (range rows))))
-
-(defn get-next-pt
-  [pt1 pt2]
-  (->> (map - pt2 pt1)
-       (mapv + pt2)))
+      {} (range rows))))
 
 (defn get-antinodes
   [board [pt1 pt2]]
-  (let [an1 (get-next-pt pt1 pt2)
-        an2 (get-next-pt pt2 pt1)]
+  (let [an1 (mapv + pt2 (map - pt2 pt1))
+        an2 (mapv + pt1 (map - pt1 pt2))]
     (reduce (fn [acc an]
               (if (get-in board an)
                 (conj acc an) acc))
@@ -66,9 +46,8 @@
 
 (defn q1
   [board]
-  (->> (get-all-antenna board)
-       (mapcat #(-> (get-pts board %)
-                    (get-permutation 2)))
+  (->> (vals (get-all-antenna board))
+       (mapcat #(get-permutation % 2))
        (reduce (fn [acc comb]
                  (into acc (get-antinodes board comb)))
                #{})
@@ -76,7 +55,7 @@
 
 #_(q1 in)
 
-(defn get-next-pt2
+(defn get-next-pts
   [board pt1 pt2]
   (let [d (map - pt2 pt1)]
     (loop [pt pt2
@@ -88,14 +67,13 @@
 
 (defn get-antinodes2
   [board [pt1 pt2]]
-  (into (get-next-pt2 board pt1 pt2)
-        (get-next-pt2 board pt2 pt1)))
+  (into (get-next-pts board pt1 pt2)
+        (get-next-pts board pt2 pt1)))
 
 (defn q2
   [board]
-  (->> (get-all-antenna board)
-       (mapcat #(-> (get-pts board %)
-                    (get-permutation 2)))
+  (->> (vals (get-all-antenna board))
+       (mapcat #(get-permutation % 2))
        (reduce (fn [acc comb]
                  (into acc (get-antinodes2 board comb)))
                #{})
