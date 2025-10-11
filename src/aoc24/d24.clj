85c3a754ddbe3676f9b1de6b7018b8a26bf5f3b3
Mark Hwang <hwangma@gmail.com>
2025-10-11 02:54:59 -0400

    day 24

--- src/aoc24/d24.clj
+++ src/aoc24/d24.clj
@@ -6,149 +6,131 @@
 
 (defn parse-input [s]
   (let [sl (get-lines s)
-        vm (->> (take-while (comp seq s/trim) sl)
-                (map #(let [[k v] (s/split % #":")]
-                        (let [v (atoi (s/trim v))]
-                          [(s/trim k) ["AND" v v]])))
-                (into {}))]
-    (->> (drop (inc (count vm)) sl)
-         (map #(let [[a op b _ k] (s/split % #" ")]
-                 [(s/trim k) [(s/trim op) (s/trim a) (s/trim b)]]))
-         (into vm))))
+        m (->> (take-while (comp seq s/trim) sl)
+               (map #(let [[k v] (s/split % #":")]
+                       (let [v (atoi (s/trim v))]
+                         [(s/trim k) v])))
+               (into {}))
+        wires (->> (drop (inc (count m)) sl)
+                   (map #(let [[a op b _ k] (s/split % #" ")]
+                           [(s/trim k) [(s/trim op) (s/trim a) (s/trim b)]]))
+                   (into {}))]
+    [m wires]))
 
 (def in (parse-input (get-res)))
 
 (defn get-value
-  ([m k]
-   (get-value m k {}))
-  ([m k swaps]
-   (let [v (get m k)]
-     (if (vector? v)
-       (let [[op a b] v
-             [m av] (if (string? a) (get-value m a swaps) [m a])
-             [m bv] (if (string? b) (get-value m b swaps) [m b])
-             op (case op "OR" bit-or "XOR" bit-xor "AND" bit-and)
-             v (op av bv)
-             m (assoc m k v)]
-         (if-let [sk (get swaps k)]
-           (let [[sm sv] (get-value m sk (dissoc swaps sk))]
-             [(assoc sm k sv sk v) sv])
-           [m v]))
-       [m v]))))
+  [m wires k]
+  (if-let [v (get m k)]
+    [m v]
+    (when-let [[op a b] (get wires k)]
+      (let [[m av] (get-value m wires a)
+            [m bv] (get-value m wires b)
+            op (case op "OR" bit-or "XOR" bit-xor "AND" bit-and)
+            v (op av bv)]
+        [(assoc m k v) v]))))
 
 (defn get-all-value
-  [m c]
+  [[m wires] c]
   (loop [i 0
          m m
-         ret '()]
-    (let [[m v] (get-value m (format "%s%02d" c i))]
-      (if (nil? v)
-        (->> (apply str ret)
-             (#(Long/parseLong % 2)))
-        (recur (inc i) m (conj ret v))))))
+         ret 0]
+    (if-let [[m v] (get-value m wires (format "%s%02d" c i))]
+      (recur (inc i) m (+ ret (bit-shift-left v i)))
+      ret)))
 
 (defn q1
-  [m]
-  (get-all-value m \z))
+  [mw]
+  (get-all-value mw \z))
 
 #_(q1 in)
 
-(defn get-z-expect [m]
-  (let [x (get-all-value m \x)
-        y (get-all-value m \y)
-        z (->> (+ x y) Long/toBinaryString (map atoi) reverse vec)
-        zkeys (->> (keys m) (filter #(= \z (first %))) sort)
-        pad-len (- (count z) (count zkeys))
-        z (nth (iterate #(conj % 0) z) pad-len)]
-    (mapv vector zkeys z)))
+(defn xk [i] (format "x%02d" i))
+(defn yk [i] (format "y%02d" i))
+(defn zk [i] (format "z%02d" i))
 
-(defn num-keys [m]
-  (->> m (filter #(number? (second %))) (map first) set))
+(defn gen-map
+  ([]
+   (->> (range 45)
+        (mapcat (fn [i] [[(xk i) 0] [(yk i) 0]]))
+        (into {})))
+  ([i x y c]
+   (cond-> (gen-map)
+     true (assoc (xk i) x
+                 (yk i) y)
+     (pos? i) (assoc (xk (dec i)) c
+                     (yk (dec i)) c))))
+
+(defn get-linked
+  [wires k]
+  (loop [ks #{k}
+         ret #{k}]
+    (if (empty? ks)
+      ret
+      (let [nks (set
+                  (mapcat
+                    #(when-let [[op a b] (get wires %)]
+                       [a b])
+                    ks))]
+        (recur nks (into ret nks))))))
+
+(defn wire-groups
+  [wires]
+  (let [sw (reduce
+             (fn [sw k]
+               (let [linked (get-linked wires k)]
+                 (assoc sw k linked)))
+             {} (keys wires))
+        [wg] (reduce
+               (fn [[wg sw] i]
+                 (let [xs (keep (fn [[k es]] (when (es (xk i)) k)) sw)
+                       links (remove (fn [[k]] (#{\x \y} k)) xs)]
+                   [(assoc wg i links)
+                    (reduce dissoc sw xs)]))
+               [{} sw] (reverse (range 45)))]
+    [sw wg]))
+
+(defn check-wire
+  [wires i]
+  (->> (for [x [0 1] y [0 1] c (if (pos? i) [0 1] [0])] [x y c])
+       (every? (fn [[x y c]]
+                 (= (second (get-value (gen-map i x y c) wires (zk i)))
+                    (bit-xor c (bit-xor x y)))))))
 
 (defn get-combos [n l]
-  (->> l
-       (reduce
+  (->> (reduce
          (fn [acc i]
-           (->> (map #(conj % i) acc)
+           (->> (mapv #(conj % i) acc)
                 (into acc)
-                (filter #(<= (count %) n))))
-         [[]])
-       (filter #(= (count %) n))))
-
-(defn find-groups [m]
-  (let [zcnt (->> (keys m) (filter #(= \z (first %))) count)
-        nm (reduce
-             (fn [acc k]
-               (let [[km v] (get-value m k)
-                     nks (num-keys km)]
-                 (assoc acc k nks)))
-             {} (keys m))
-        [bm] (reduce
-               (fn [[acc nm] i]
-                 (let [xs (keep
-                            (fn [[k es]]
-                              (when (or (es (format "x%02d" i))
-                                        (es (format "y%02d" i)))
-                                k))
-                            nm)
-                       exs (remove (fn [[k]] (#{\x \y} k)) xs)]
-                   [(assoc acc (format "z%02d" i) exs)
-                    (reduce dissoc nm xs)]))
-               [{} nm] (reverse (range zcnt)))]
-    bm))
+                (filterv #(<= (count %) n))))
+         [[]] l)
+       (filterv #(= (count %) n))))
 
 (defn find-switches
-  [m groups expects swaps]
-  (loop [m m
-         expects expects
-         swaps swaps]
-      (if (empty? expects)
-        [swaps]
-        (let [[k ev] (first expects)
-              [zm v] (get-value m k swaps)]
-          (if (= v ev)
-            (recur zm (next expects) swaps)
-            (let [sl (->> (get groups k)
-                          (get-combos 2)
-                          (keep (fn [[e1 e2]]
-                                  (let [sw (into swaps [[e1 e2] [e2 e1]])]
-                                    (when (= ev (second (get-value m k sw)))
-                                      sw)))))]
-              (when (seq sl)
-                (mapcat #(find-switches m groups expects %) sl))))))))
+  [sub-wires groups wires]
+  (reduce
+    (fn [[wires swaps :as ws] i]
+      (if (check-wire wires i)
+        ws
+        (->> (get groups i)
+             (get-combos 2)
+             (some
+               (fn [[e1 e2]]
+                 (when-not (or ((sub-wires e1) e2)
+                               ((sub-wires e2) e1))
+                   (let [wires (assoc wires
+                                      e1 (get wires e2)
+                                      e2 (get wires e1))]
+                     (when (check-wire wires i)
+                       [wires (conj swaps e1 e2)]))))))))
+    [wires #{}]
+    (range 45)))
 
 (defn q2
-  [m]
-  (let [expects (get-z-expect m)
-        groups (find-groups m)]
-    (->> (find-switches m groups expects {})
-         first keys sort (s/join ","))))
+  [[_ wires]]
+  (let [[sub-wires wg] (wire-groups wires)]
+    (->> (find-switches sub-wires wg wires)
+         second sort
+         (s/join ","))))
 
-;But, I can’t find all the values, so in the end, I have to use draw-groups to find them.
 #_(q2 in)
-
-(defn sort-ops
-  [m l]
-  (->> (map #(conj (get m %) %) l)
-       (sort (fn [a b]
-               (let [[oa aa ba] a
-                     [ob ab bb] b]
-                 (cond
-                   (and (#{\x \y} (first aa))
-                        (#{\x \y} (first ab))) (if (= oa "XOR") -1 1)
-                   (#{\x \y} (first aa)) -1
-                   (#{\x \y} (first aa)) 1
-                   :else
-                   (let [ao (case oa "AND" -1 "OR" 0 "XOR" 1)
-                         bo (case oa "AND" -1 "OR" 0 "XOR" 1)]
-                     (compare ao bo))))))
-       vec))
-
-(defn draw-groups
-  [m file]
-  (doseq [[k es] (sort (find-groups m))]
-    (spit file (str k " ************************\n") :append true)
-    (doseq [[op a b k] (sort-ops m es)]
-      (spit file (str k " : " a " " op " "  b "\n") :append true))))
-
-#_(draw-groups in "wires.txt")
